e540ad14d5867b9a43a3d4477644d05ea220e9b4
Oliver Charles <oliver.g.charles@googlemail.com>
2012-10-13 00:23:46 +0100

    Add missing Data.Artist.Tests module

---
+++ test/suite/MusicBrainz/Data/Artist/Tests.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+module MusicBrainz.Data.Artist.Tests ( tests ) where
+
+import Control.Applicative
+import Data.Maybe (fromJust)
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test)
+
+import MusicBrainz
+import MusicBrainz.Data.Artist
+
+tests :: [Test]
+tests = [ testFindLatestByMbid ]
+
+testFindLatestByMbid :: Test
+testFindLatestByMbid = testCase "findLatestByMbid when artist exists" $ do
+  mbTest (findLatestByMbid knownArtistId) >>= (@?= expected)
+  where
+    knownArtistId = fromJust $ parseMbid "206094f7-eea0-4f37-a4c2-97c506f5f560"
+    expected = Just $
+      CoreEntity { coreMbid = knownArtistId
+                 , coreRevision = RevisionRef 1
+                 , coreData =
+                     Artist { artistName = "Massive Attack"
+                            , artistSortName = "Massive Attack"
+                            , artistComment = ""
+                            , artistBeginDate = emptyDate
+                            , artistEndDate = emptyDate
+                            , artistEnded = False
+                            , artistGender = Nothing
+                            , artistCountry = Nothing
+                            , artistType = Nothing
+                            }
+                 }
+
+mbTest :: MusicBrainz a -> IO a
+mbTest = runMB databaseSettings
+  where databaseSettings = defaultConnectInfo { connectDatabase = "musicbrainz_nes"
+                                              , connectUser = "musicbrainz"
+                                              }
