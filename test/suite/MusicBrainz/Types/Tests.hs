d625ec6217b6ed839d17b64ed1b0fcc6d87c40cd
Oliver Charles <oliver.g.charles@googlemail.com>
2012-10-13 12:09:17 +0100

    Add more tests to parsing MBIDs and asserting when partial dates are empty

--- test/suite/MusicBrainz/Types/Tests.hs
+++ test/suite/MusicBrainz/Types/Tests.hs
@@ -2,9 +2,12 @@ module MusicBrainz.Types.Tests
     ( tests ) where
 
 import Control.Applicative
+import Data.Maybe (isNothing)
 import Test.Framework
+import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
+import Test.HUnit hiding (Test)
 
 import qualified Data.UUID as UUID
 
@@ -14,11 +17,35 @@ instance Arbitrary UUID.UUID where
   arbitrary = UUID.fromWords <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
 
 tests :: [Test]
-tests = [ testParseMbid ]
+tests = [ testGroup "MBID"
+            [ testParseMbid
+            , testParseInvalidMbid
+            ]
+        , testGroup "PartialDate"
+            [ testEmptyPartialDates
+            , testNonEmptyPartialDates
+            ]
+        ]
 
 --------------------------------------------------------------------------------
-{-| Converting a UUID into a string and parsing it produces an MBID, whos
-inner UUID is the same as the input. -}
 testParseMbid :: Test
 testParseMbid = testProperty "Can parse UUIDs as MBIDs" $
   \uuid -> parseMbid (UUID.toString uuid) == Just (MBID uuid)
+
+
+testParseInvalidMbid :: Test
+testParseInvalidMbid = testProperty "Non UUID strings do not parse" $
+  \s -> isNothing (UUID.fromString s) ==> parseMbid s == Nothing
+
+--------------------------------------------------------------------------------
+testNonEmptyPartialDates :: Test
+testNonEmptyPartialDates =
+  testProperty "When any field is set, the date is not empty" $
+    \y m d -> any (not . isNothing) [y, m, d] ==>
+                not (isEmpty $ PartialDate y m d)
+
+testEmptyPartialDates :: Test
+testEmptyPartialDates =
+  testCase "When all fields are Nothing, the date is empty" $
+    assertBool "emptyPartial date is empty"
+      (isEmpty $ PartialDate Nothing Nothing Nothing)
