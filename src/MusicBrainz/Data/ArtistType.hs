dc325e7ce4715c7844b1f9167b10f46b61aa0cd4
Oliver Charles <ollie@ocharles.org.uk>
2012-11-05 12:39:42 +0000

    Add MusicBrainz.Data.(ArtistType|Gender)

---
+++ src/MusicBrainz/Data/ArtistType.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-| Functions to work with 'ArtistType's. -}
+module MusicBrainz.Data.ArtistType ( addArtistType ) where
+
+import Control.Applicative
+import Database.PostgreSQL.Simple.SqlQQ (sql)
+
+import MusicBrainz hiding (artistType)
+
+--------------------------------------------------------------------------------
+{-| Add a new 'ArtistType' to the list of known countries in MusicBrainz. -}
+addArtistType :: ArtistType -> MusicBrainz (Entity ArtistType)
+addArtistType artistType = head <$>
+  query [sql| INSERT INTO artist_type (name) VALUES (?)
+              RETURNING id, name |] artistType
---
+++ src/MusicBrainz/Data/Gender.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-| Functions to work with 'Gender's. -}
+module MusicBrainz.Data.Gender ( addGender ) where
+
+import Control.Applicative
+import Database.PostgreSQL.Simple.SqlQQ (sql)
+
+import MusicBrainz
+
+--------------------------------------------------------------------------------
+{-| Add a new 'Gender' to the list of known countries in MusicBrainz. -}
+addGender :: Gender -> MusicBrainz (Entity Gender)
+addGender gender = head <$>
+  query [sql| INSERT INTO gender (name) VALUES (?)
+              RETURNING id, name |] gender
