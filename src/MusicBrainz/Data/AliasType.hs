aaf1ea08c1c24975502305eeb0bca74597d0285e
Oliver Charles <ollie@ocharles.org.uk>
2013-01-25 23:51:48 +0000

    Add ResolveReference/Add instances for artist and label alias types

--- src/MusicBrainz/Data/AliasType.hs
+++ src/MusicBrainz/Data/AliasType.hs
@@ -14,6 +14,32 @@ import MusicBrainz.Data.Add
 import MusicBrainz.Data.FindLatest
 
 --------------------------------------------------------------------------------
+instance Add (AliasType Artist) where
+  add type' = head <$>
+    query [sql| INSERT INTO artist_alias_type (name) VALUES (?)
+                RETURNING id, name |] type'
+
+
+--------------------------------------------------------------------------------
+instance ResolveReference (AliasType Artist) where
+  resolveReference aliasTypeId = listToMaybe . map fromOnly <$>
+    query [sql| SELECT id FROM artist_alias_type WHERE id = ? |] (Only aliasTypeId)
+
+
+--------------------------------------------------------------------------------
+instance Add (AliasType Label) where
+  add type' = head <$>
+    query [sql| INSERT INTO label_alias_type (name) VALUES (?)
+                RETURNING id, name |] type'
+
+
+--------------------------------------------------------------------------------
+instance ResolveReference (AliasType Label) where
+  resolveReference aliasTypeId = listToMaybe . map fromOnly <$>
+    query [sql| SELECT id FROM label_alias_type WHERE id = ? |] (Only aliasTypeId)
+
+
+--------------------------------------------------------------------------------
 instance Add (AliasType Work) where
   add type' = head <$>
     query [sql| INSERT INTO work_alias_type (name) VALUES (?)
