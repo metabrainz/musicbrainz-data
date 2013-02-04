07a7e229d5a041dbd3c99238fb8ee9a9b5bf7c12
Oliver Charles <ollie@ocharles.org.uk>
2013-02-04 17:02:52 +0000

    Add 'Add' and 'ResolveReference' instances for 'ReleaseStatus'

--- src/MusicBrainz/Data/ReleaseStatus.hs
+++ src/MusicBrainz/Data/ReleaseStatus.hs
@@ -1,17 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-| Functions to work with 'ReleaseStatus'. -}
-module MusicBrainz.Data.ReleaseStatus ( addReleaseStatus ) where
+module MusicBrainz.Data.ReleaseStatus ( ) where
 
 import Control.Applicative
+import Data.Maybe (listToMaybe)
+import Database.PostgreSQL.Simple (Only(..))
 import Database.PostgreSQL.Simple.SqlQQ (sql)
 
 import MusicBrainz
+import MusicBrainz.Data.Add
+import MusicBrainz.Data.FindLatest
 
 --------------------------------------------------------------------------------
-{-| Add a new 'ReleaseStatus' to the list of known release statuses in
-MusicBrainz.-}
-addReleaseStatus :: ReleaseStatus -> MusicBrainz (Entity ReleaseStatus)
-addReleaseStatus rs = head <$>
-  query [sql| INSERT INTO release_status (name) VALUES (?)
-              RETURNING id, name |] rs
+instance Add ReleaseStatus where
+  add rs = head <$>
+    query [sql| INSERT INTO release_status (name) VALUES (?)
+                RETURNING id, name |] rs
+
+
+--------------------------------------------------------------------------------
+instance ResolveReference ReleaseStatus where
+  resolveReference releaseStatusId = listToMaybe . map fromOnly <$>
+    query [sql| SELECT id FROM release_status WHERE id = ? |] (Only releaseStatusId)
