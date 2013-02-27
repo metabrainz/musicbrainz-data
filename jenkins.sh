6b81a0ca150f685b775cc86e23623786274e8760
Oliver Charles <ollie@ocharles.org.uk>
2013-02-27 09:08:02 +0000

    Add jenkins build configuration

---
+++ jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export PATH=/usr/local/postgres/bin:$PATH
+cabal update
+
+if ! grep -q $(md5sum test/data/test-schema.sql) .schema-version
+then
+  psql -U postgres -c "DROP DATABASE musicbrainz_nes"
+  psql -U postgres -c "CREATE DATABASE musicbrainz_nes"
+  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/cube.sql
+  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/uuid-ossp.sql
+  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/musicbrainz_collate.sql
+  psql -U postgres musicbrainz_nes < test/data/test-schema.sql
+fi
+md5sum test/data/test-schema.sql > .schema-version
+
+cabal clean
+cabal install --enable-tests --only-dependencies --force-reinstalls
+cabal configure --enable-tests
+cabal build
+
+cabal test --test-option='--jxml=junit.xml -j5'
