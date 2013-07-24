00f291335d73793ac50f175829ec9254dfea6bae
Oliver Charles <ollie@ocharles.org.uk>
2013-07-24 16:10:39 +0000

    jenkins.sh: Use CREATE EXTENSION

--- jenkins.sh
+++ jenkins.sh
@@ -6,9 +6,9 @@ if ! grep -q $(md5sum test/data/test-schema.sql) .schema-version
 then
   psql -U postgres -c "DROP DATABASE musicbrainz_nes"
   psql -U postgres -c "CREATE DATABASE musicbrainz_nes"
-  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/cube.sql
-  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/uuid-ossp.sql
-  psql -U postgres musicbrainz_nes < /usr/local/postgres/share/contrib/musicbrainz_collate.sql
+  psql -U postgres musicbrainz_nes -c "CREATE EXTENSION cube"
+  psql -U postgres musicbrainz_nes -c 'CREATE EXTENSION "uuid-ossp"'
+  psql -U postgres musicbrainz_nes -c 'CREATE EXTENSION musicbrainz_collate'
   psql -U postgres musicbrainz_nes < test/data/test-schema.sql
 fi
 md5sum test/data/test-schema.sql > .schema-version
